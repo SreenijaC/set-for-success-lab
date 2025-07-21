16fadeff64712895e309df7195612a906496ac23
Sreenija C <anupama130706@gmail.com>
2025-07-20 21:46:45 -0500

    updated with comments

--- app/src/main/java/org/example/RemoveDuplicate.java
+++ app/src/main/java/org/example/RemoveDuplicate.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class RemoveDuplicate {// removes duplicate entries from an arraylist using set
+public class RemoveDuplicate {// duplicate entries are removed from arraylist using set
 
-    // takes generic arraylist and removes any duplicates
+    // generic arraylist is taken and any duplicates if are removed
     public static <T> ArrayList<T> removeDuplicates(ArrayList<T> inputList) {
         // linkedhashset used to remove duplicates which keeps same order as original list
         Set<T> uniqueSet = new LinkedHashSet<>(inputList);
